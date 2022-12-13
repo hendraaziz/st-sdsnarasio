ffe8d8daa30d8ad7bb992d448f52a5c114857620
hendraaziz <hendraaziz80@gmail.com>
2022-12-13 11:59:30 +0700

    update sidebar

--- main.py
+++ main.py
@@ -19,15 +19,42 @@ warnings.filterwarnings('always')
 warnings.filterwarnings('ignore')
 
 
-st.write("""
-## PROJECT - Senior Data Scientist [Recommendation System]
-### Study Case Restaurant
----
-#### Aziz Hendra Atmaja
----
-""")
-
 option = st.sidebar.selectbox(
     'Silakan pilih:',
-    ('Home', 'Dataframe', 'Modeling', 'Recommendation')
+    ('Home', 'Dataset', 'Modeling', 'Recommendation')
 )
+
+if option == 'Home' or option == '':
+    st.write("""# Halaman Utama""")  # menampilkan halaman utama
+    st.write("""
+    ## PROJECT - Senior Data Scientist [Recommendation System]
+    ### Study Case Restaurant
+    ---
+    #### Aziz Hendra Atmaja
+    ---
+    """)
+elif option == 'Dataset':
+    st.write("""## Read Dataset""")  # menampilkan judul halaman dataframe
+
+    # membuat dataframe dengan pandas yang terdiri dari 2 kolom dan 4 baris data
+    # df = pd.DataFrame({
+    #    'Column 1':[1,2,3,4],
+    #    'Column 2':[10,12,14,16]
+    # })
+    # df #menampilkan dataframe
+elif option == 'Modeling':
+    st.write("""## Draw Modeling""")  # menampilkan judul halaman
+
+    # membuat variabel chart data yang berisi data dari dataframe
+    # data berupa angka acak yang di-generate menggunakan numpy
+    # data terdiri dari 2 kolom dan 20 baris
+    # chart_data = pd.DataFrame(
+    #    np.random.randn(20,2),
+    #    columns=['a','b']
+    # )
+    # menampilkan data dalam bentuk chart
+    # st.line_chart(chart_data)
+    # data dalam bentuk tabel
+    # chart_data
+elif option == 'Recommendation':
+    st.write("""## Recommendation System""")  # menampilkan judul halaman
